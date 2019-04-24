67a2aea407c6667d08696fbce6755aef623683b3
italo alcantara <italo.ads31@gmail.com>
2019-04-24 06:03:38 -0300

    implemented combo list dynamic and message of success when save a title

--- src/main/java/com/italo/titulos/Titulos/controller/TituloController.java
+++ src/main/java/com/italo/titulos/Titulos/controller/TituloController.java
@@ -1,11 +1,17 @@
 package com.italo.titulos.Titulos.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.italo.titulos.Titulos.model.TipoStatus;
 import com.italo.titulos.Titulos.model.Titulo;
 import com.italo.titulos.Titulos.service.TituloService;
 
@@ -17,13 +23,16 @@ public class TituloController {
 	private TituloService tituloService;
 	
 	@GetMapping
-	public String inxe() {
-		return "CadastroTitulo";
+	public ModelAndView index(Titulo titulo) {
+		return tituloService.Titulos(titulo);
 	}
 	
 	@PostMapping
-	public String salvar(Titulo titulo) {
+	public ModelAndView salvar(Titulo titulo) {
 		return tituloService.salvar(titulo);
 	}
-	
+	@ModelAttribute("tipoStatus")
+	public List<TipoStatus> tipoStatusTitulo(){
+		return Arrays.asList(TipoStatus.values());
+	}
 }
--- src/main/java/com/italo/titulos/Titulos/service/TituloService.java
+++ src/main/java/com/italo/titulos/Titulos/service/TituloService.java
@@ -1,13 +1,17 @@
 package com.italo.titulos.Titulos.service;
 
-import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.italo.titulos.Titulos.model.TipoStatus;
 import com.italo.titulos.Titulos.model.Titulo;
 import com.italo.titulos.Titulos.repository.TituloRepository;
 
@@ -15,14 +19,25 @@ import com.italo.titulos.Titulos.repository.TituloRepository;
 @Transactional
 public class TituloService {
 	
+	private static final String VIEW_CADASTRO=("CadastroTitulo");
+	
 	@Autowired
 	private TituloRepository tituloRepository;
 	
-	public String salvar(Titulo titulo) {
-		Titulo tituloDB =titulo;
+	public ModelAndView salvar(Titulo titulo) {
+		
+		ModelAndView mv =new ModelAndView(VIEW_CADASTRO);
+		mv.addObject("menssagem","titulo salvo com sucesso!");
+		Titulo tituloDB=titulo;
 		tituloDB.setDataInclusao(Calendar.getInstance());
 		tituloRepository.save(tituloDB);
-		return "CadastroTitulo";
+		return mv;
 	}
 
+	public ModelAndView Titulos(Titulo titulo) {
+		ModelAndView mv = new ModelAndView(VIEW_CADASTRO);
+		return mv;
+	}
+	
+	
 }
